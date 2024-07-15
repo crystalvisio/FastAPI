a276e48610b3d4b00d3b0b8c3196b330e03a0f92
crystalvisio <crystalvisio@gmail.com>
2024-07-15 16:41:40 +0300

    Routers Modularization

---
+++ app/routers/post.py
@@ -0,0 +1,70 @@
+from fastapi import Response, status, Depends, APIRouter
+from typing import List
+from sqlalchemy.orm import Session
+
+from .. import models, schemas, utils
+from ..database import get_db
+
+
+# Initialise App Router
+router = APIRouter()
+
+
+# Reading All...
+@router.get("/post", response_model=List[schemas.PostBase])
+def get_posts(db: Session = Depends(get_db)):
+    posts = db.query(models.Post).all()
+    return {"data": posts}
+
+
+# Reading One...
+@router.get("/get-post/{id}")
+def get_post(id:int, response:Response, db: Session = Depends(get_db)):
+
+    post = db.query(models.Post).filter(models.Post.id==id).first()
+
+    if not post:
+        utils.id_error("Post", id)
+    return {"Post_details":post}
+
+
+# Creating...
+@router.post("/create-post", status_code=status.HTTP_201_CREATED, response_model=schemas.PostResponse)
+def create_posts(post:schemas.PostBase, db: Session = Depends(get_db)):
+
+    new_post = models.Post(**post.model_dump())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post) # return new post
+
+    return new_post
+
+
+# Deleting...
+@router.delete("/delete/{id}")
+def delete_post(id:int, db: Session=Depends(get_db)):
+
+    post = db.query(models.Post).filter(models.Post.id==id)
+
+    if post.first() == None:
+        utils.id_error("Post", id)
+
+    post.delete(synchronize_session=False)
+    db.commit()
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+# Updating...
+@router.put("/update/{id}")
+def update_post(id:int, updated_post:schemas.PostBase, db:Session=Depends(get_db)):
+
+    post = models.Post(**updated_post.model_dump()).filter(models.Post.id==id)
+
+    if post.first() == None:
+        utils.id_error("Post", id)
+    
+    post.update(updated_post.model_dump(), synchronize_session=False)
+    db.commit()
+
+    return {"Updated Post":post.first()}
---
+++ app/routers/user.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
+from sqlalchemy.orm import Session
+
+from .. import models, schemas, utils
+from ..database import get_db
+
+
+# Initialise App Router
+router = APIRouter()
+
+
+# Creating User
+@router.post("/create-user", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
+def create_users(user:schemas.UserCreate, db: Session = Depends(get_db)):
+
+    # hashing pwd - user.password
+    user.password = utils.hash_pwd(user.password) 
+
+    new_user = models.User(**user.model_dump())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user) # return  new user
+
+    return new_user
+
+
+# Getting User by id
+@router.get("/get-user/{id}", response_model=schemas.UserGet)
+def get_post(id:int, db: Session = Depends(get_db)):
+
+    user = db.query(models.User).filter(models.User.id==id).first()
+
+    if not user:
+        utils.id_error("User", id)
+
+    return user
